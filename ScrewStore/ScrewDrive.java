596d26773c86bfe7d872ef8aa0625fc70ea2d85a
NoDairy.co <151561762+nodairyco@users.noreply.github.com>
2023-11-22 00:36:41 +0400

    Update ScrewDrive.java
    made it like the exercise

--- ScrewStore/ScrewDrive.java
+++ ScrewStore/ScrewDrive.java
@@ -1,5 +1,5 @@
 public enum ScrewDrive {
-    SlotDrive, Cross, Hex, Torx;
+    /*SlotDrive, Cross, Hex, Torx;
     @Override
     public String toString(){
         return switch(this){
@@ -8,7 +8,38 @@ public enum ScrewDrive {
             case Torx -> "Torx";
             case SlotDrive -> "Slot Drive";
         };
-    }
-    //I understand this does nothing,
-    //but at the same time I don't know the Georgian translations
+    }*/
+    //Though this worked Helmut said: "Fruitless be thine endevourse!", and made me do this:
+    SlotDrive {
+        @Override
+        public String toString() {
+            return "schlitz";
+        }
+    },
+
+    Cross {
+        @Override
+        public String toString() {
+            return "kreuzschlitz";
+        }
+    },
+
+    Hex {
+        @Override
+        public String toString() {
+            return "sechskant";
+        }
+    },
+
+    Torx {
+        @Override
+        public String toString() {
+            return "torx";
+        }
+    };
+
+    @Override
+    public abstract String toString();
 }
+
+
